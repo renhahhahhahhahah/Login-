2781721623a5acc9240c987c2d06b3f5a03f690b
renhahhahhahhahah <154959721+renhahhahhahhahah@users.noreply.github.com>
2024-05-21 23:33:41 +0800

    Add files via upload

---
+++ Login/Login/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelcontainer = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panellog = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Txtbxloguser = new System.Windows.Forms.TextBox();
+            this.Txtbxlogpass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtbxSuusername = new System.Windows.Forms.TextBox();
+            this.TxtbxSuCpass = new System.Windows.Forms.TextBox();
+            this.TxtbxSuPass = new System.Windows.Forms.TextBox();
+            this.Signupbtn = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panelcontainer.SuspendLayout();
+            this.panellog.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelcontainer
+            // 
+            this.panelcontainer.Controls.Add(this.richTextBox1);
+            this.panelcontainer.Controls.Add(this.label5);
+            this.panelcontainer.Controls.Add(this.label4);
+            this.panelcontainer.Controls.Add(this.button1);
+            this.panelcontainer.Controls.Add(this.Txtbxlogpass);
+            this.panelcontainer.Controls.Add(this.Txtbxloguser);
+            this.panelcontainer.Controls.Add(this.pictureBox1);
+            this.panelcontainer.Controls.Add(this.label3);
+            this.panelcontainer.Location = new System.Drawing.Point(35, 27);
+            this.panelcontainer.Name = "panelcontainer";
+            this.panelcontainer.Size = new System.Drawing.Size(308, 388);
+            this.panelcontainer.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Papyrus", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(398, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 76);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TearDown";
+            // 
+            // panellog
+            // 
+            this.panellog.BackColor = System.Drawing.Color.Transparent;
+            this.panellog.Controls.Add(this.linkLabel1);
+            this.panellog.Controls.Add(this.label2);
+            this.panellog.Location = new System.Drawing.Point(411, 247);
+            this.panellog.Name = "panellog";
+            this.panellog.Size = new System.Drawing.Size(266, 16);
+            this.panellog.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Dont have any account?";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(170, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(43, 13);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Sign-up";
+            this.toolTip1.SetToolTip(this.linkLabel1, "Sign-up");
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(120, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 27);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Login";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Login.Properties.Resources.three_people_clipart_removebg_preview;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(115, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(73, 60);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Txtbxloguser
+            // 
+            this.Txtbxloguser.Location = new System.Drawing.Point(78, 167);
+            this.Txtbxloguser.Name = "Txtbxloguser";
+            this.Txtbxloguser.Size = new System.Drawing.Size(167, 20);
+            this.Txtbxloguser.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.Txtbxloguser, "Enter your username");
+            // 
+            // Txtbxlogpass
+            // 
+            this.Txtbxlogpass.Location = new System.Drawing.Point(78, 213);
+            this.Txtbxlogpass.Name = "Txtbxlogpass";
+            this.Txtbxlogpass.PasswordChar = '•';
+            this.Txtbxlogpass.Size = new System.Drawing.Size(167, 20);
+            this.Txtbxlogpass.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.Txtbxlogpass, "Enter your password");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.toolTip1.SetToolTip(this.button1, "Login");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Username";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(77, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Password\r\n";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 900;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.Signupbtn);
+            this.panel1.Controls.Add(this.TxtbxSuPass);
+            this.panel1.Controls.Add(this.TxtbxSuCpass);
+            this.panel1.Controls.Add(this.TxtbxSuusername);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(363, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(355, 388);
+            this.panel1.TabIndex = 3;
+            this.panel1.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(138, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 27);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Sign-up";
+            // 
+            // TxtbxSuusername
+            // 
+            this.TxtbxSuusername.Location = new System.Drawing.Point(84, 109);
+            this.TxtbxSuusername.Name = "TxtbxSuusername";
+            this.TxtbxSuusername.Size = new System.Drawing.Size(214, 20);
+            this.TxtbxSuusername.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.TxtbxSuusername, "Enter your username");
+            // 
+            // TxtbxSuCpass
+            // 
+            this.TxtbxSuCpass.Location = new System.Drawing.Point(84, 200);
+            this.TxtbxSuCpass.Name = "TxtbxSuCpass";
+            this.TxtbxSuCpass.PasswordChar = '•';
+            this.TxtbxSuCpass.Size = new System.Drawing.Size(214, 20);
+            this.TxtbxSuCpass.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.TxtbxSuCpass, "Confirm your password");
+            // 
+            // TxtbxSuPass
+            // 
+            this.TxtbxSuPass.Location = new System.Drawing.Point(84, 158);
+            this.TxtbxSuPass.Name = "TxtbxSuPass";
+            this.TxtbxSuPass.PasswordChar = '•';
+            this.TxtbxSuPass.Size = new System.Drawing.Size(214, 20);
+            this.TxtbxSuPass.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.TxtbxSuPass, "Enter your password");
+            // 
+            // Signupbtn
+            // 
+            this.Signupbtn.Location = new System.Drawing.Point(155, 271);
+            this.Signupbtn.Name = "Signupbtn";
+            this.Signupbtn.Size = new System.Drawing.Size(82, 23);
+            this.Signupbtn.TabIndex = 6;
+            this.Signupbtn.Text = "Sign-up";
+            this.toolTip1.SetToolTip(this.Signupbtn, "Sign-up");
+            this.Signupbtn.UseVisualStyleBackColor = true;
+            this.Signupbtn.Click += new System.EventHandler(this.Signupbtn_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(81, 93);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Username";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(81, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Password";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(81, 184);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Confirm Password";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(80, 292);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(165, 93);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(766, 455);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panellog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelcontainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelcontainer.ResumeLayout(false);
+            this.panelcontainer.PerformLayout();
+            this.panellog.ResumeLayout(false);
+            this.panellog.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelcontainer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panellog;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txtbxlogpass;
+        private System.Windows.Forms.TextBox Txtbxloguser;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button Signupbtn;
+        private System.Windows.Forms.TextBox TxtbxSuPass;
+        private System.Windows.Forms.TextBox TxtbxSuCpass;
+        private System.Windows.Forms.TextBox TxtbxSuusername;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Login/Login/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string filepath = @"C:\\Users\\ME\\Documents\\Finalsprog\containerdata.txt";
+        ArrayList allusername = new ArrayList();
+        Dictionary<string, string> userAndpass = new Dictionary<string, string>();
+
+        public Dictionary<string, string> MyDictionary
+        {
+            get { return userAndpass; }
+            set { userAndpass = value; }
+        }
+        public void Getuser()
+        {
+            string[] lines = File.ReadAllLines(filepath);
+            string[] values;
+            string username = "";
+            string password = "";
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                values = lines[i].ToString().Split(':');
+
+                if (values[0].Trim().Equals("Username"))
+                {
+                    username = values[1];
+                    allusername.Add(username);
+                }
+                else if (values[0].Trim().Equals("SignPass"))
+                {
+                    password = values[1];
+                }
+                if (username != "" && password != "")
+                {
+                    userAndpass.Add(username,password);
+                    richTextBox1.Text += username + " - " + password;
+                    richTextBox1.Text += Environment.NewLine;
+                    username = "";
+                    password = "";
+                }
+            }
+        }
+
+        private void Signupbtn_Click(object sender, EventArgs e)
+        {
+            Getuser();
+            string Signupuser = TxtbxSuusername.Text;
+            string Signuppass = TxtbxSuPass.Text;
+            string SignupCompass = TxtbxSuCpass.Text;
+
+            TextWriter write = new StreamWriter(filepath, true);
+            write.Write("Username: " + Signupuser);
+            write.WriteLine("");
+            write.Write("Signpass: " + Signuppass);
+            write.WriteLine("");
+            write.Write("Confirm Pass: " + SignupCompass);
+            write.WriteLine("");
+
+            write.Close();
+            MessageBox.Show("Account created");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string loguname = Txtbxloguser.Text;
+            string logpassword = Txtbxlogpass.Text;
+            bool userExist = false;
+            if (loguname.Trim().Equals("") || logpassword.Trim().Equals(""))
+            {
+                MessageBox.Show("you need to enter the username or Password");
+            }
+            else
+            {
+                foreach (var use in userAndpass)
+                {
+                    if (use.Key.ToString().Trim().Equals(loguname))
+                    {
+                        if (use.Value.ToString().Trim().Equals(logpassword))
+                        {
+
+                            userExist = true;
+                            break;
+                        }
+                    }
+                }
+                if (userExist)
+                {
+                    MessageBox.Show("yes");
+                }
+                else
+                {
+                    MessageBox.Show("no");
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            panel1.Show();
+        }
+    }
+}
+//reference:https://www.youtube.com/watch?v=1z9OeGAHbfI&t=12s
